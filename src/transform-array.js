821bb294ce9f36eaf34cbee3569b2611183725c4
AllainDM <78161319+AllainDM@users.noreply.github.com>
2021-09-13 00:22:17 +0300

    55

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,26 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  let newArr = [];
+  for (let x = 0; x < arr.length; x++) {
+      if ( arr[x] == '--discard-next') {
+          x += 2;
+      } else if ( arr[x] == '--discard-prev') {
+          newArr.splice(x - 1, 1)
+      } else if ( arr[x] == '--double-next') {
+          newArr.push(arr[x + 1])
+      } else if ( arr[x] == '--double-prev') {
+          newArr.push(arr[x - 1])
+      } else {
+          newArr.push(arr[x]);
+      }
+  }
+  return newArr;
+
 }
+
+// export default function transform(/* arr */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
