1d415792df40028c567b45c4520365fda9b72145
AllainDM <78161319+AllainDM@users.noreply.github.com>
2021-09-14 09:09:32 +0300

    56

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,23 +6,63 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default {
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+    return this;
+},
+addLink(value) {
+    if (typeof arr1 === 'undefined') {
+        arr1 = [];
+    };
+    arr1.push(`( ${value} )`);
+    return this;
+},
+removeLink(position) {
+    arr1.splice(position - 1, 1)
+    return this;
+},
+reverseChain() {
+    if (typeof arr1 === 'undefined') {
+      arr1 = [];
+    };
+    arr1.reverse();
+    return this;
+},
+finishChain() {
+    for (let x = 0; x < arr1.length - 1; x++) {
+        if (typeof result === 'undefined') {
+            result = '';
+        };
+        result += arr1[x] + '~~';
+    }
+    result += arr1[arr1.length - 1];
+    newResult = result;
+    result = '';
+    // console.log(arr);
+    // console.log(newResult);
+    arr1 = [];
+    return this;
+}
 };
+
+// export default {
+//   getLength() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   },
+//   addLink(/* value */) {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   },
+//   removeLink(/* position */) {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   },
+//   reverseChain() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   },
+//   finishChain() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   }
+// };
--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,33 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr) {
+  let newArr = [];
+  arr.forEach(e => {
+      if (e > 0) {
+          newArr.push(e);
+      }
+      
+  });
+  newArr.sort(function(a, b) {
+    return a - b;
+  }); 
+  let result = [];
+  arr.forEach((e, i) => {
+      if (e > 0) {
+          result[i] = newArr[0];
+          newArr.splice(0, 1);
+      } else {
+          result[i] = arr[i];
+      }
+      
+  });
+  return result;
 }
+
+
+
+// export default function sortByHeight(/* arr */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
